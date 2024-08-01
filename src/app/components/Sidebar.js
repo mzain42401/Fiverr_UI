4dfd83bb5a3ce5e69340359fadb6adccfaf427ca
m zain ali <mzain42401@gmail.com>
2024-08-01 19:53:20 +0500

    update

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -2,19 +2,23 @@ import { useState } from 'react';
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
-    <div className={` fixed md:relative inset-y-0 left-0 z-50  w-56 bg-gray-800 text-white transform md:top-0 top-[62px] ${isOpen ? 'translate-x-0 ' : '-translate-x-full'} transition-transform lg:translate-x-0`}>
-      <div className="flex flex-col h-full">
-        <div className="flex-1 p-4 space-y-2">
+    <div className={` fixed md:sticky inset-y-0 left-0 z-50  w-56 bg-gray-800 text-white transform  md:top-0 top-[56px]  ${isOpen ? 'translate-x-0 ' : '-translate-x-full'} transition-transform lg:translate-x-0`}>
+      <div className="flex flex-col h-full ">
+        <div className='hidden lg:block'><h1 className=' h-16 flex justify-center items-center'>LOGO</h1></div>
+        <div className="flex-1 p-4 space-y-1">
           
           <a href="#" className="block p-2 bg-gray-700 rounded text-center ">Dashboard</a>
-          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Sales</a>
-          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Analytics</a>
-          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Messages</a>
-          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Settings</a>
+          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Idea Generation</a>
+          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Content Generation</a>
+          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Editing</a>
+          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Scheduling</a>
+          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Subscription</a>
+
+          
         </div>
         <div className="p-4 space-y-2 border-t border-gray-700">
-          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Account</a>
           <a href="#" className="block p-2 bg-gray-700 rounded text-center">Settings</a>
+          <a href="#" className="block p-2 bg-gray-700 rounded text-center">Help</a>
         </div>
       </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -12,11 +12,16 @@ const Page = () => {
   };
 
   return (
+    <>
+    
     <div className="flex h-screen overflow-hidden">
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="flex-1 flex flex-col">
-        <header className="p-4 bg-gray-800 text-white lg:hidden">
-          <button onClick={toggleSidebar} className="focus:outline-none">
+      <header className="sticky p-4 flex justify-between lg:justify-center items-center bg-gray-800 text-white ">
+          <h1 className='lg:hidden block'>Logo</h1>
+          <input className='flex-2' type="text" />
+          
+          <button onClick={toggleSidebar} className="focus:outline-none lg:hidden">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={sidebarOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}></path>
             </svg>
@@ -27,6 +32,7 @@ const Page = () => {
         </main>
       </div>
     </div>
+    </>
   );
 };
 
